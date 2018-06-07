b7f33076393b9f911e44bc16cf7920b0a87be9f2
yuanxx <824264439@qq.com>
2018-06-07 11:09:40 +0800

    增加开放接口功能的实现层级

---
+++ luckydeer-note/luckydeer-note-service/src/main/java/cn/luckydeer/note/service/note/NoteInfoServiceImpl.java
@@ -0,0 +1,21 @@
+package cn.luckydeer.note.service.note;
+
+import java.util.List;
+
+import cn.luckydeer.note.facade.note.NoteInfoService;
+import cn.luckydeer.note.model.note.NoteInfoModel;
+
+/**
+ * 
+ * 开放接口功能的实现层
+ * @author yuanxx
+ * @version $Id: NoteInfoServiceImpl.java, v 0.1 2018年6月7日 上午11:08:57 yuanxx Exp $
+ */
+public class NoteInfoServiceImpl implements NoteInfoService {
+
+    @Override
+    public List<NoteInfoModel> selectAll() {
+        return null;
+    }
+
+}
