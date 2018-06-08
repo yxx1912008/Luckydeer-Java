7e06c17fdc9f6441409f0bc8d4cd514d754e1e14
yuanxx <824264439@qq.com>
2018-06-08 15:46:17 +0800

    增加缓存系统常量，用于减轻 Memcached缓存压力 ，最大缓存时间当前为三十天

---
+++ luckydeer-common/luckydeer-memcached/src/main/java/cn/luckydeer/memcached/constants/CachedConstants.java
@@ -0,0 +1,14 @@
+package cn.luckydeer.memcached.constants;
+
+/**
+ * 缓存系统常用 常量
+ * 
+ * @author yuanxx
+ * @version $Id: CachedConstants.java, v 0.1 2018年6月8日 下午3:44:25 yuanxx Exp $
+ */
+public interface CachedConstants {
+
+    /** 最大缓存时间   当前设置为30天 */
+    int MAX_CACHE_TIME = 60 * 60 * 24 * 30;
+
+}
