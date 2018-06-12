550ecfe8b081dd7c69518517ef912f4212bce3ce
yuanxx <824264439@qq.com>
2018-06-12 14:16:23 +0800

    完善微信控制器

--- luckydeer-member/luckydeer-weixin/src/main/java/cn/luckydeer/weixin/user/WxController.java
+++ luckydeer-member/luckydeer-weixin/src/main/java/cn/luckydeer/weixin/user/WxController.java
@@ -1,9 +1,18 @@
 package cn.luckydeer.weixin.user;
 
+import java.util.List;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import cn.luckydeer.common.enums.ViewShowEnums;
+import cn.luckydeer.common.model.ResponseObj;
+import cn.luckydeer.dao.users.daoInterface.IWxUserDao;
+import cn.luckydeer.dao.users.dataobject.WxUserDo;
 
 /**
  * 
@@ -16,13 +25,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping(value = "/wx", method = RequestMethod.POST)
 public class WxController {
 
-    @RequestMapping(value = "/getAllUser", produces = "application/json;charset=utf-8")
-    @ResponseBody
-    public void name() {
+    @Autowired
+    private IWxUserDao wxUserDao;
 
-        
-        
-        
+    @RequestMapping(value = "/getAllUser.do", produces = "application/json;charset=utf-8")
+    @ResponseBody
+    public String getAllUser() {
+
+        List<WxUserDo> list = wxUserDao.selectAll();
+        if (CollectionUtils.isEmpty(list)) {
+            return new ResponseObj(ViewShowEnums.ERROR_FAILED.getCode(),
+                ViewShowEnums.ERROR_FAILED.getDetail()).toJson();
+        }
+        return new ResponseObj(list).toJson();
     }
 
 }
