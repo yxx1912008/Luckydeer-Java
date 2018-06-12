3c65bf06b30338f5adacfe779e51e7703da95c4a
yuanxx <824264439@qq.com>
2018-06-12 14:48:53 +0800

    修改方法拦截器，用于拦截Dao操作

--- luckydeer-common/luckydeer-common/src/main/java/cn/luckydeer/common/filter/DalMonitorInterceptor.java
+++ luckydeer-common/luckydeer-common/src/main/java/cn/luckydeer/common/filter/DalMonitorInterceptor.java
@@ -4,6 +4,8 @@ import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
 import org.apache.log4j.Logger;
 
+import com.alibaba.fastjson.JSON;
+
 /**
  * 
  * 监控数据库操作
@@ -32,17 +34,64 @@ public class DalMonitorInterceptor implements MethodInterceptor {
      */
     @Override
     public Object invoke(MethodInvocation invocation) throws Throwable {
-        System.out.println("拦截器起作用");
         /** 获取包名 */
         String packageName = invocation.getMethod().getDeclaringClass().getPackage().getName();
         /** 获取类名  */
         String className = invocation.getMethod().getDeclaringClass().getSimpleName();
         String method = new StringBuilder(packageName).append(".").append(className).append(".")
             .append(invocation.getMethod().getName()).toString();
+
+        Object[] args = invocation.getArguments();
+
+        String params = null;
+
+        if (null != args && args.length > 0) {
+            params = JSON.toJSONString(args);
+        }
+
+        long startTime = System.currentTimeMillis();
+
+        String severity = "[MIN],";
+
+        boolean hasError = false;
         dalMonitorLogger.info("拦截开始:" + method);
-        Object result = invocation.proceed();
-        dalMonitorLogger.info("拦截结束:" + result.toString());
-        return result;
+        try {
+            return invocation.proceed();
+        } catch (Throwable e) {
+            dalMonitorLogger.error(method, e);
+            hasError = true;
+            throw e;
+        } finally {
+
+            long elapseTime = System.currentTimeMillis() - startTime;
+            if (elapseTime < minTime) {
+                severity = "[MIN],";
+            } else if (elapseTime >= minTime && elapseTime < 1000) {
+                severity = "[MID],";
+            } else if (elapseTime >= 1000 && elapseTime < 10000) {
+                severity = "[MAX],";
+            } else {
+                severity = "[XMA],";
+            }
+
+            StringBuilder builder = new StringBuilder("(");
+            builder.append(method);
+
+            if (hasError) {
+                builder.append(",N,").append(severity).append(elapseTime).append("ms) ;args=")
+                    .append(params);
+                dalMonitorLogger.info(builder.toString());
+            }
+            // 无异常时不打印300毫秒以下的监控日志
+            if (!hasError && elapseTime >= minTime) {
+                builder.append(",Y,").append(severity).append(elapseTime).append("ms)");
+                if (elapseTime >= 1000) {
+                    builder.append(" ;args=").append(params);
+                }
+                dalMonitorLogger.info(builder.toString());
+            }
+
+        }
     }
 
 }
